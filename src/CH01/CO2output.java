e31ae4fc31fd5c2bcd7c3254a949ccc800ffd2bb
IT KOREA <gjwlsgur123@gmail.com>
2022-09-30 16:04:20 +0900

    0930

---
+++ src/CH01/CO2output.java
@@ -0,0 +1,29 @@
+package CH01;
+
+public class CO2output {
+
+	public static void main(String[] args) {
+		//System.out : 표준 출력 스트림
+		//01 System.out.print()
+		//ESCAPE=탈출 
+		//ESCAPE 문자사용
+		// \n : 개행문자
+		// \b : 백스페이스
+		
+		System.out.print("     *");
+		System.out.print("\n    ***");
+		System.out.print("\n   *****");
+		System.out.print("\n  *******");
+		
+		//문제
+		// 		*
+		// 	   ***
+		//    *****
+		//   *******
+		//  *********
+		
+		//System.out.print();
+		//System.out.println();
+	}
+
+}
